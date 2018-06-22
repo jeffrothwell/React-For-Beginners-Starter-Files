d1c1645095036ddb14c48bb1a893ac8379a69848
jeffrothwell <rothwelljeff@gmail.com>
2018-06-22 12:25:23 -0400

    add button to load sample fishes into State

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes })
+  }
 
   addFish = (fish) => {
     // copy current state
@@ -25,7 +30,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-      <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     )
   }
