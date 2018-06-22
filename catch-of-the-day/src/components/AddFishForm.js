52176a6cbb3a4efcfebe3deed88382c8b45c992e
jeffrothwell <rothwelljeff@gmail.com>
2018-06-22 10:36:18 -0400

    create simple add fish form and render in Inventory

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+  render() {
+    return (
+      <form className="fish-edit">
+        <input name="name" type="text" placeholder="Name" />
+        <input name="price" type="text" placeholder="Price" />
+        <input name="status" type="text" placeholder="Status" />
+        <textarea name="desc" type="text" placeholder="Desc" />
+        <input name="image" type="text" placeholder="Image" />
+        <button type="submit">+ Add Fish</button>
+      </form>
+    )
+  }
+}
+
+export default AddFishForm;
