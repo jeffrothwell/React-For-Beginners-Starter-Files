0683f70b46853b193dfe88ea00e24db76243811f
jeffrothwell <rothwelljeff@gmail.com>
2018-06-22 12:03:24 -0400

    create fish object state, allow form to add fish to state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,20 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = (fish) => {
+    // copy current state
+    const fishes = {...this.state.fishes};
+    // add our new fish
+    fishes[`fish${Date.now()}`] = fish;
+    // set the new fishes in our object to state
+    this.setState({ fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +25,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+      <Inventory addFish={this.addFish} />
       </div>
     )
   }
